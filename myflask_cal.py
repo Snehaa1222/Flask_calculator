e9dd1d773e939f713c6ce00156f4e5632d3633f9
Sneha Gandhi <gandhisneha99@gmail.com>
2022-12-24 20:08:03 +0530

    initialize flask project

---
+++ myflask_cal.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def home_page():
+    return "Home Page"
+
+
+@app.route("/math", methods=["POST"])
+def calculator():
+    if request.method == "POST":
+        operation = request.json['operation']
+        num1 = int(request.json["num1"])
+        num2 = int(request.json["num2"])
+
+        result = ""
+
+        if operation == 'add':
+            r = num1 + num2
+            result = "The sum is: " + str(r)
+
+        if operation == 'sub':
+            r = num1 - num2
+            result = "The subtraction is: " + str(r)
+
+        if operation == 'mul':
+            r = num1 * num2
+            result = "The multiplication is: " + str(r)
+
+        if operation == 'divide':
+            r = num1 / num2
+            result = "The division is: " + str(r)
+
+        return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
